c1b768b040f7d9c2f807861bf681248acfb05550
USer <priyankasmhetre06@gmail.com>
2021-03-16 11:21:39 +0530

    createprofile added and update profile added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sppu_student_application/Screens/CreateProfile.dart';
-import 'package:sppu_student_application/Screens/Createprofile1.dart';
-import 'package:sppu_student_application/Screens/profile.dart';
+
 import 'package:sppu_student_application/Screens/updateProfile.dart';
 
 
@@ -11,7 +10,7 @@ class myapp extends StatelessWidget{
   @override
   Widget build(BuildContext context) => MaterialApp(
     debugShowCheckedModeBanner: false,
-      home:updateProfile(),
+      home:Createprofile(),
     );
 
 }
