2a7c4165cd626b074b49208e43a6e5eb4ce7169d
vikasvarnekar3 <vikasvarnekar3@gmail.com>
2021-03-31 13:16:27 +0530

    added signup page

--- lib/Screens/Admin_signup.dart
+++ lib/Screens/Admin_signup.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 class Admin_signup extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<StatefulWidget> createState() =>_Admin_signup();
 
+
+}
+
+class _Admin_signup extends State <Admin_signup> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title:Text("Admin Signup"),
+   ),
+    body: Center(
+   child: Column(
+    children : <Widget>[
+      Text("SignUp"),
+    RaisedButton(
+    child: Text("Submit"),
+    onPressed: (){
+      Navigator.pop(context);
+       }
+      )
+     ]
+     )
+    )
+   );
+  }
 }
